e521d9b1ebc3165be8ea9caa08fd9dc2ecdcf3ee
Ray1239 <3277mohammedusmani@gmail.com>
2024-01-26 23:49:33 +0530

    testing three.js

--- animate_algorithm/src/App.js
+++ animate_algorithm/src/App.js
@@ -1,9 +1,34 @@
 // import logo from './logo.svg';
 import './App.css';
 // import React, { useState } from 'react';
-import Maze from './components/Maze';
+// import Maze from './components/Maze';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
@@ -11,7 +36,13 @@ function App() {
       <Navbar title="Maze Solving Game" />
       <div className="mainCont">
         <div className="mazeSpace">
-          <Maze/>
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
         </div>
         <div className="codingSpace">
           <h1>This is code space</h1>
